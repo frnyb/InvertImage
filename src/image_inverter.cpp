d561ab3ccf7905c141c29e5fcd518d44f0943f2c
frnyb <ffn@mmmi.sdu.dk>
2022-01-23 19:52:01 +0100

    Works on the board!

--- src/image_inverter.cpp
+++ src/image_inverter.cpp
@@ -36,6 +36,18 @@ int main(int argc, char * argv[])
 
         success = XInvert_image_Initialize(&x_ii, "invert_image");
 
+	if (success == XST_DEVICE_NOT_FOUND)
+	{
+		std::cout << "Device not found\n\r";
+		while(1);
+	}
+
+	if (success == XST_OPEN_DEVICE_FAILED)
+	{
+		std::cout << "Open device failed\n\r";
+		while(1);
+	}
+
         if (success != XST_SUCCESS)
         {
             std::cout << "Component initialization failed!\n\r";
@@ -47,7 +59,7 @@ int main(int argc, char * argv[])
 
         std::cout << "Creating test data\n\r";
 
-        char *data_send = (char *)malloc(SIZE);
+        uint8_t *data_send = (uint8_t *)malloc(SIZE);
 
         if (data_send == NULL)
         {
@@ -64,19 +76,19 @@ int main(int argc, char * argv[])
         std::cout << "Finished initializing data.\n\r";
 
         std::cout << "First 5 entries of data:\n\r";
-        std::cout << data_send[0]
-            << "\t" << data_send[1]
-            << "\t" << data_send[2]
-            << "\t" << data_send[3]
-            << "\t" << data_send[4]
+        std::cout << (int)data_send[0]
+            << "\t" << (int)data_send[1]
+            << "\t" << (int)data_send[2]
+            << "\t" << (int)data_send[3]
+            << "\t" << (int)data_send[4]
             << std::endl;
 
         std::cout << "Last 5 entries of data:\n\r";
-        std::cout << data_send[SIZE-1]
-            << "\t" << data_send[SIZE-2]
-            << "\t" << data_send[SIZE-3]
-            << "\t" << data_send[SIZE-4]
-            << "\t" << data_send[SIZE-5]
+        std::cout << (int)data_send[SIZE-1]
+            << "\t" << (int)data_send[SIZE-2]
+            << "\t" << (int)data_send[SIZE-3]
+            << "\t" << (int)data_send[SIZE-4]
+            << "\t" << (int)data_send[SIZE-5]
             << std::endl;
 
         std::cout << "Polling for invert_image ready...\n\r";
@@ -91,7 +103,7 @@ int main(int argc, char * argv[])
     {
         std::this_thread::sleep_for(std::chrono::milliseconds(1000));
 
-        reply = XInvert_image_Write_image_in_Bytes(&x_ii, 0, data_send, SIZE);
+        reply = XInvert_image_Write_image_in_Bytes(&x_ii, 0, (char *)data_send, SIZE);
 
         std::cout << "Wrote %u bytes of data.\n\r", reply;
 
@@ -111,7 +123,7 @@ int main(int argc, char * argv[])
 
         std::cout << "Reading image out data...\n\r";
 
-        char *image_out_ptr = (char *)malloc(SIZE);
+        uint8_t *image_out_ptr = (uint8_t *)malloc(SIZE);
 
         if (image_out_ptr == NULL)
         {
@@ -119,24 +131,24 @@ int main(int argc, char * argv[])
             while(1);
         }
 
-        reply = XInvert_image_Read_image_out_Bytes(&x_ii, 0, image_out_ptr, SIZE);
+        reply = XInvert_image_Read_image_out_Bytes(&x_ii, 0, (char *)image_out_ptr, SIZE);
 
         std::cout << "Read %u bytes of data.\n\r", reply;
 
         std::cout << "First 5 entries of data:\n\r";
-        std::cout << data_send[0]
-            << "\t" << image_out_ptr[1]
-            << "\t" << image_out_ptr[2]
-            << "\t" << image_out_ptr[3]
-            << "\t" << image_out_ptr[4]
+        std::cout << (int)image_out_ptr[0]
+            << "\t" << (int)image_out_ptr[1]
+            << "\t" << (int)image_out_ptr[2]
+            << "\t" << (int)image_out_ptr[3]
+            << "\t" << (int)image_out_ptr[4]
             << std::endl;
 
         std::cout << "Last 5 entries of data:\n\r";
-        std::cout << data_send[SIZE-1]
-            << "\t" << image_out_ptr[SIZE-2]
-            << "\t" << image_out_ptr[SIZE-3]
-            << "\t" << image_out_ptr[SIZE-4]
-            << "\t" << image_out_ptr[SIZE-5]
+        std::cout << (int)image_out_ptr[SIZE-1]
+            << "\t" << (int)image_out_ptr[SIZE-2]
+            << "\t" << (int)image_out_ptr[SIZE-3]
+            << "\t" << (int)image_out_ptr[SIZE-4]
+            << "\t" << (int)image_out_ptr[SIZE-5]
             << std::endl;
 
     }
